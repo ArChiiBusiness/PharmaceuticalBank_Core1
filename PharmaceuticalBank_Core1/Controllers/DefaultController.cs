c1cdcd6a989b69334dd58b783b7322265a85e000
Artūrs Jaunošāns <Arturs.Jaunosans@edu.rtu.lv>
2020-04-12 20:20:03 +0300

    Add project files.

---
+++ PharmaceuticalBank_Core1/Controllers/DefaultController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PharmaceuticalBank_Core1.Models.DAL;
+
+namespace PharmaceuticalBank_Core1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DefaultController : ControllerBase
+    {
+
+        public IActionResult GetValue() {
+
+            using (var context = new excelpro_pharmabankContext()) {
+                return new JsonResult(context.AspNetUsers.ToList());
+            }
+        }
+
+    }
+}
---
+++ PharmaceuticalBank_Core1/Models/DAL/Suppliers.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PharmaceuticalBank_Core1.Models.DAL
+{
+    public partial class Suppliers
+    {
+        public Guid Id { get; set; }
+        public string Type { get; set; }
+        public string SupplierName { get; set; }
+    }
+}
